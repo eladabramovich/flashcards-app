f0f72c6440ba0092fa3ab95059fd08835330064b
Elad Ab <abramovich.elad@gmail.com>
2021-05-30 14:24:37 +0300

    Added reset quiz view

--- src/pages/QuizPage/QuizPage.tsx
+++ src/pages/QuizPage/QuizPage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useState } from 'react';
 import { FlashcardsContext } from '../../context/flashcards-context';
 
 import Container from '../../components/UI/Container/Container';
@@ -11,13 +11,9 @@ import { Flashcard } from '../../types/flashcards-types';
 import styles from './QuizPage.module.css';
 
 const QuizPage = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
   const { cardsState, cardsDispatch } = useContext(FlashcardsContext);
-
-  useEffect(() => {
-    if (cardsState.items.length === 0) {
-      cardsDispatch({ type: 'LOAD', payload: musicCards as Flashcard[] });
-    }
-  }, [cardsState, cardsDispatch]);
+  let content = null;
 
   const clickHandler = (id: string) => {
     if (cardsState.items[0].correctAnswer === id) {
@@ -27,26 +23,73 @@ const QuizPage = () => {
     }
   };
 
+  const startQuiz = () => {
+    if (cardsState.items.length === 0) {
+      cardsDispatch({ type: 'LOAD', payload: musicCards as Flashcard[] });
+    }
+    setIsPlaying(true);
+  };
+
+  const resetQuiz = () => {
+    cardsDispatch({ type: 'RESET' });
+    setIsPlaying(true);
+  };
+
+  if (isPlaying && cardsState.items.length >= 1) {
+    content = cardsState.items.map((item) => {
+      if (cardsState.items[0].id === item.id) {
+        return (
+          <div key={item.id}>
+            <FlashCardQuestion
+              question={item.question}
+              summary={item.summary}
+            />
+            <FlashCardAnswers answers={item.answers} onClicked={clickHandler} />
+          </div>
+        );
+      } else {
+        return null;
+      }
+    });
+  } else if (isPlaying && cardsState.items.length <= 0) {
+    content = (
+      <div className={`${styles.btnCont} ${styles.resetView}`}>
+        <button className={styles.quizBtn} onClick={() => resetQuiz()}>
+          Reset Quiz
+        </button>
+      </div>
+    );
+  } else {
+    content = (
+      <div className={styles.instructions}>
+        <h1>Instructions</h1>
+        <h3>Quiz yourself:</h3>
+        <p>
+          To start, click the button at the end of these insturctions. After
+          that you will be presented with a question and four possible answers
+          to choose from. If you are unsure what is the right answer you can
+          hover over/touch the question card to reveal a short summary. When you
+          have an answer in mind, click on the answer card to check and if you
+          are correct you will be moved to the next flashcard question.
+        </p>
+        <h3>Manage cards:</h3>
+        <p>
+          To add or remove flashcards from the deck, click on "Manage cards"
+          link there you will see a short form that allows you to create new
+          flashcards and a list of exising cards.
+        </p>
+        <div className={styles.btnCont}>
+          <button className={styles.quizBtn} onClick={() => startQuiz()}>
+            Start quiz
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <main className={styles.quizPage}>
-      {cardsState.items.map((item) => {
-        if (cardsState.items[0].id === item.id) {
-          return (
-            <Container key={item.id}>
-              <FlashCardQuestion
-                question={item.question}
-                summary={item.summary}
-              />
-              <FlashCardAnswers
-                answers={item.answers}
-                onClicked={clickHandler}
-              />
-            </Container>
-          );
-        } else {
-          return null;
-        }
-      })}
+      <Container>{content}</Container>
     </main>
   );
 };
