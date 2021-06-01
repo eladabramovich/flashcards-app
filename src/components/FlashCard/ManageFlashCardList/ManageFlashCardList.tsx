3c68f0581b5af576f642958a0b19530a634b39a5
Elad Ab <abramovich.elad@gmail.com>
2021-06-01 13:53:38 +0300

    Created card list in manage cards page

---
+++ src/components/FlashCard/ManageFlashCardList/ManageFlashCardList.tsx
@@ -0,0 +1,35 @@
+import styles from './ManageFlashCardList.module.css';
+
+import { Flashcard } from '../../../types/flashcards-types';
+
+type Props = {
+  cards: Flashcard[];
+};
+
+const ManageFlashCardList = ({ cards }: Props) => {
+  console.log(cards);
+  return (
+    <div className={styles.manageFlashCardList}>
+      {cards.map((card) => (
+        <div key={card.id} className={styles.item}>
+          <h3 className={styles.question}>{card.question}</h3>
+          <button className={styles.deleteBtn}>delete</button>
+        </div>
+      ))}
+      <div className={styles.item}>
+        <h3 className={styles.question}>Question</h3>
+        <button className={styles.deleteBtn}>delete</button>
+      </div>
+      <div className={styles.item}>
+        <h3 className={styles.question}>Question</h3>
+        <button className={styles.deleteBtn}>delete</button>
+      </div>
+      <div className={styles.item}>
+        <h3 className={styles.question}>Question</h3>
+        <button className={styles.deleteBtn}>delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default ManageFlashCardList;
--- src/pages/ManageCardsPage/ManageCardsPage.tsx
+++ src/pages/ManageCardsPage/ManageCardsPage.tsx
@@ -1,13 +1,21 @@
+import { useContext } from 'react';
+import { FlashcardsContext } from '../../context/flashcards-context';
+
 import Container from '../../components/UI/Container/Container';
+
 import AddFlashCardForm from '../../components/FlashCard/AddFlashCardForm/AddFlashCardForm';
+import ManageFlashCardList from '../../components/FlashCard/ManageFlashCardList/ManageFlashCardList';
 
 import styles from './ManageCardsPage.module.css';
 
 const ManageCardsPage = () => {
+  const { cardsState } = useContext(FlashcardsContext);
   return (
     <main>
       <Container>
+        <h1 className={styles.title}>Manage Flashcards</h1>
         <AddFlashCardForm />
+        <ManageFlashCardList cards={cardsState.items} />
       </Container>
     </main>
   );
